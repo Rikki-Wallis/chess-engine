0af11958d1fc7a82f9082d8494b4d6b99319537e
RikkiWallisMonash <rikkiwallis2004@gmail.com>
2024-12-06 20:13:20 +1100

    Further implemented main. Now works being able to move pieces and switching colours each time a piece is moved

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 import pygame
-from pygame._sdl2 import Window
-
 from board import Board
 from gui import GUI
+from cursor import Cursor
+from tile import Tile
+from piece import Piece
+from rook import Rook
 from constants import *
+import copy
+import math
+from pygame._sdl2 import Window
+from game import Game
 
 # Initialising the game board
 gameBoard = Board()
@@ -14,5 +20,44 @@ gui = GUI()
 # Drawing the board to the screen
 gui.drawBoard()
 
+clock = pygame.time.Clock()
+
+cursor = Cursor(gui)
+
+game = Game(gameBoard, gui, cursor)
+
+currentColour = 'w'
+
+# Drawing the board to the screen
+gui.drawBoard()
+gui.drawPieces(gameBoard.board)
+    
 while True:
-    gui.drawPieces(gameBoard.board)
+    clock.tick(60)
+
+    # Event Handler
+    for event in pygame.event.get():
+        # If the player decides to quit out of the window
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        
+        # If the mouse button is pressed
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            game.handleClick(currentColour)
+            # # While the mouse button is pressed down
+            # while pygame.mouse.get_pressed()[0]:
+        
+    # If a move has been made switch the current colours turn
+    if game.hasMoveBeenMade == True:
+        currentColour = 'w' if currentColour == 'b' else 'b'
+        game.hasMoveBeenMade = False
+                
+        
+
+
+        
+
+
+    
+    
